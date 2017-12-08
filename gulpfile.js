e3df0d17dc9ec69f901f32f92af8c84ba2451d4a
Rodrigo Cintrao <rodrigo.cintrao@gmail.com>
2017-12-08 14:50:47 -0200

    first files

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var jshint = require('gulp-jshint');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var es = require('event-stream');
+var imagemin = require('gulp-imagemin');
+var changed  = require('gulp-changed');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+var runSequence = require('run-sequence');
+
+gulp.task('images', function() {
+    gulp.src(['.dev/images/**/*.jpg','.dev/images/**/*.png'])
+        .pipe(changed('./img/'))
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('./img/'));
+});
+
+gulp.task('clean', function(){
+  return gulp.src('./js/')
+  .pipe(clean());
+});
+
+gulp.task('jshint', function(){
+  return gulp.src('.dev/scripts/**/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+gulp.task('uglify', function(){
+  return es.merge([
+    gulp.src(['bower_components/jquery/dist/jquery.min.js', 'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js', 'bower_components/slick-carousel/slick/slick.min.js']),
+    gulp.src('.dev/scripts/**/*.js').pipe(uglify())
+  ])
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('.dev/scss/**/*.scss')
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function(){
+  return gulp.watch('.dev/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('serve', function () {
+    var files = [
+      './style.css',
+      './**/*.php ',
+      './**/*.js'
+    ];
+    browserSync.init(files, {
+        proxy: 'http://localhost/joy-storm-pub',
+        notify: false
+    });
+
+    gulp.watch('*').on('change', reload);
+});
+
+gulp.task('default', function(cb){
+  return runSequence('clean', ['images','jshint','sass','watch','uglify','serve'], cb);
+});
